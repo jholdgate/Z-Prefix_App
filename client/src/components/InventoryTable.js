2dd13fbb87be5c58826210da81da6efddc166e40
jholdgate <jholdgate1@yahoo.com>
2024-08-07 20:04:05 -0400

    implement editable name/quantity/description fields

--- client/src/components/InventoryTable.js
+++ client/src/components/InventoryTable.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
 
 
 const InventoryTable = ({ newItem }) => {
@@ -27,19 +29,31 @@ const InventoryTable = ({ newItem }) => {
     }
   };
 
-  const actionButtons = (rowData) => {
-    return (
-      <React.Fragment>
-        {/* <Button icon="pi pi-pencil" className="p-button-rounded p-button-success mr-2" onClick={() => editItem(rowData)} /> */}
-        <Button icon="pi pi-trash" className="p-button-rounded p-button-warning" onClick={() => deleteItem(rowData)} />
-      </React.Fragment>
-    );
+  const onRowEditComplete = async (e) => {
+    let { newData, index } = e;
+
+    try {
+      const res = await fetch(`http://localhost:8080/items/${newData.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newData),
+        credentials: 'include'
+      });
+
+      if (res.ok) {
+        let newItems = [...items];
+        newItems[index] = newData;
+        setItems(newItems);
+      } else {
+        const errorData = await res.json();
+        console.error('Error updating item:', errorData.error);
+      }
+    } catch (err) {
+      console.error('Error updating item:', err);
+    }
   };
-
-  // const editItem = (item) => {
-
-  //   console.log('Edit item:', item)
-  // }
 
   const deleteItem = async (item) => {
     try {
@@ -59,18 +73,39 @@ const InventoryTable = ({ newItem }) => {
     }
   }
 
-  return (
-        <div>
-            <h1>Inventory Table</h1>
-            <DataTable value={items} >
-                <Column field="id" header="ID"></Column>
-                <Column field="item_name" header="Item Name"></Column>
-                <Column field="quantity" header="Quantity"></Column>
-                <Column field="description" header="Description"></Column>
-                <Column body={actionButtons} exportable={false} style={{ minWidth: '8rem' }}></Column>
-            </DataTable>
-        </div>
+  const textEditor = (options) => {
+    return <InputText type="text" value={options.value} onChange={(e) => options.editorCallback(e.target.value)} />;
+  };
+
+  const numberEditor = (options) => {
+    return <InputNumber value={options.value} onValueChange={(e) => options.editorCallback(e.value)} />;
+  };
+
+  const actionButtons = (rowData) => {
+    return (
+      <React.Fragment>
+        <Button icon="pi pi-trash" className="p-button-rounded p-button-warning" onClick={() => deleteItem(rowData)} />
+      </React.Fragment>
     );
-}
+  };
+
+  return (
+    <div>
+      <h1>Inventory Table</h1>
+      <DataTable
+        value={items}
+        editMode="row"
+        dataKey="id"
+        onRowEditComplete={onRowEditComplete}
+      >
+        <Column field="item_name" header="Item Name" editor={(options) => textEditor(options)} />
+        <Column field="quantity" header="Quantity" editor={(options) => numberEditor(options)} />
+        <Column field="description" header="Description" editor={(options) => textEditor(options)} />
+        <Column rowEditor header="Edit" headerStyle={{ width: '10%', minWidth: '8rem' }} bodyStyle={{ textAlign: 'center' }} />
+        <Column header="Delete" body={actionButtons} exportable={false} style={{ minWidth: '8rem' }} />
+      </DataTable>
+    </div>
+  );
+};
 
 export default InventoryTable;
